161518164cf3ee6d2fd992617d87aaa016ab319c
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-09-29 16:28:29 +0200

    in progress

--- app/components/mini_cards.jsx
+++ app/components/mini_cards.jsx
@@ -1,3 +1,18 @@
+import Image from 'next/image'
+
 export default function MiniCards(image, texte) {
-  return <div className="w-40 h-40 bg-amber-600 rounded-2xl"></div>
+  return (
+    <div className="w-[170px] h-fit flex flex-col bg-crusta-300 text-crusta-900 rounded-2xl">
+      <Image
+        src="/images/cognition-color.svg"
+        alt="Icone de téléphone pour indiquer le numéro de téléphone"
+        width="150"
+        height="150"
+        loading="lazy"
+        priority={false}
+        className="object-scale-down text-start col-span mx-auto"
+      />
+      <p className="text-center text-wrap px-2">Thérapie centrée sur les émotions hohohohoho</p>
+    </div>
+  )
 }
