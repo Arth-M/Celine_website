142181702843aa97a69f7f24b42613c088794ced
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-10-14 17:02:07 +0200

    in progress

--- app/components/hero_image.jsx
+++ app/components/hero_image.jsx
@@ -4,15 +4,21 @@ import CardBelle from './card_belle'
 export default function HeroImage() {
   return (
     <div className="w-screen h-fit relative">
-      <Image
-        src="/images/consultation.webp"
-        alt="Une consultation chez le psychologue. Le patient parle de ses émotions, des évènements qu'il vit et la psychologue l'accompagne"
-        priority={true}
-        className="w-screen h-fit object-cover 2xl:object-top opacity-50"
-        // fill={true}
-        width="1521"
-        height="787"
-      />
+      <picture>
+        <source
+          srcSet="/images/consultation_lg_screen.png"
+          media="(min-width: 1024px)"
+        />
+        <Image
+          src="/images/consultation.webp"
+          alt="Une consultation chez le psychologue. Le patient parle de ses émotions, des évènements qu'il vit et la psychologue l'accompagne"
+          priority={true}
+          className="w-screen h-fit object-cover 2xl:object-top opacity-50"
+          // fill={true}
+          width="1521"
+          height="787"
+        />
+      </picture>
       <CardBelle />
     </div>
   )
