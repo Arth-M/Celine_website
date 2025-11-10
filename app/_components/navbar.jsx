e925f98ed326fecba3be57b52c211d168d380c7f
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-11-10 11:39:05 +0100

    in progress

--- app/_components/navbar.jsx
+++ app/_components/navbar.jsx
@@ -99,7 +99,7 @@ export default function Navbar() {
               alt="Indication sur la localisation du cabinet"
               width="30"
               height="30"
-              preload={true}
+              preload="true"
               className="object-scale-down border-r-4 lg:object-cover"
             />
             <p
